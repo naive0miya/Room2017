cb82e727319634542eba1fb2116ad86b7163e788
naive0miya <zdd910@outlook.com>
2017-12-11 21:09:05 +0800

    ForeignKey and Test

--- app/src/main/java/com/example/bill/room2017/AppDatabase.kt
+++ app/src/main/java/com/example/bill/room2017/AppDatabase.kt
@@ -3,10 +3,12 @@ package com.example.bill.room2017
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import com.example.bill.room2017.dao.*
+import com.example.bill.room2017.entity.Category
+import com.example.bill.room2017.entity.Library
 import com.example.bill.room2017.entity.Repo
 import com.example.bill.room2017.entity.User
 
-@Database(entities = arrayOf(User::class, Repo::class),
+@Database(entities = arrayOf(User::class, Repo::class, Category::class, Library::class),
           version = 1,
           exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
@@ -14,6 +16,6 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun repoDao() : RepoDao
     abstract fun userDao() : UserDao
     abstract fun userWithReposDao() : UserWithReposDao
-
-
+    abstract fun categoryDao() : CategoryDao
+    abstract fun libraryDao() : LibraryDao
 }
